ca116c6673fb1a3bc6701a67329223e92715a706
ycy <15703500211@163.com>
2020-07-22 16:30:47 +0800

    用户控制层的提交

---
+++ src/main/java/com/dj/hotel/web/UserController.java
@@ -0,0 +1,13 @@
+package com.dj.hotel.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户控制层
+ * @author 杨承雨
+ */
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+}
