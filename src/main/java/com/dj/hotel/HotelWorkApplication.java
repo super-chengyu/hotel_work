4f565f02fe9ba6a949ed4f4d970aa34cf689926a
ycy <15703500211@163.com>
2020-07-22 14:58:46 +0800

    启动类及配置文件的修改

--- src/main/java/com/dj/hotel/HotelWorkApplication.java
+++ src/main/java/com/dj/hotel/HotelWorkApplication.java
@@ -1,9 +1,18 @@
 package com.dj.hotel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @author 杨承雨
+ */
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.dj.hotel.mapper")
+@ServletComponentScan("com.dj.ssm.config")
 public class HotelWorkApplication {
 
     public static void main(String[] args) {
