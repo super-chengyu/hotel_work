fa12f1f21c047ed796417093cce5a8670db7f2c9
ycy <15703500211@163.com>
2020-07-22 16:30:27 +0800

    用户实体类的提交

---
+++ src/main/java/com/dj/hotel/pojo/User.java
@@ -0,0 +1,37 @@
+package com.dj.hotel.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @author 杨承雨
+ */
+@Data
+@TableName("user")
+public class User {
+
+    /** 用户主键ID */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /** 用户姓名 */
+    private String userName;
+
+    /** 用户密码 */
+    private String userPwd;
+
+    /** 用户邮箱 */
+    private String userEmail;
+
+    /** 用户手机号 */
+    private String userPhone;
+
+    /** 用户职务 */
+    private Integer userLevel;
+
+    /** 用户伪删除 */
+    private Integer isDel;
+
+}
