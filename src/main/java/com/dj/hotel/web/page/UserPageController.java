30d5f32b630c76a4ab450df38e928c2807950abc
ycy <15703500211@163.com>
2020-07-22 16:31:16 +0800

    用户页面控制层的提交

---
+++ src/main/java/com/dj/hotel/web/page/UserPageController.java
@@ -0,0 +1,13 @@
+package com.dj.hotel.web.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户页面控制层
+ * @author 杨承雨
+ */
+@Controller
+@RequestMapping("/user/")
+public class UserPageController {
+}
