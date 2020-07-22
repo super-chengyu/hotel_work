4270a6709d54f10df6f9edc3c25aa37869411b68
ycy <15703500211@163.com>
2020-07-22 14:49:54 +0800

    init commit

---
+++ src/main/java/com/dj/hotel/HotelWorkApplication.java
@@ -0,0 +1,13 @@
+package com.dj.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dj/hotel/HotelWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
