afc08cb8f54b6fcf2ff5b98f7990f3e1fab3c51f
ycy <15703500211@163.com>
2020-07-22 16:31:30 +0800

    用户页service层的提交

---
+++ src/main/java/com/dj/hotel/service/UserService.java
@@ -0,0 +1,11 @@
+package com.dj.hotel.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.dj.hotel.pojo.User;
+
+/**
+ * 用户service
+ * @author 杨承雨
+ */
+public interface UserService extends IService<User> {
+}
