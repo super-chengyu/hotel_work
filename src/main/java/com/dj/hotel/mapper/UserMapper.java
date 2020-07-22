b82e94a93cb9322e7442d734056effc540fd42a1
ycy <15703500211@163.com>
2020-07-22 16:31:03 +0800

    用户mapper的提交

---
+++ src/main/java/com/dj/hotel/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.dj.hotel.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dj.hotel.pojo.User;
+
+/**
+ * 用户mapper
+ * @author 杨承雨
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
