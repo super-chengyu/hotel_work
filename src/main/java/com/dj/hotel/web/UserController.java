0da35beea01ebbf599ae7c131d4f06172c46bd36
ycy <15703500211@163.com>
2020-07-23 09:03:41 +0800

    分支引用用户service

--- src/main/java/com/dj/hotel/web/UserController.java
+++ src/main/java/com/dj/hotel/web/UserController.java
@@ -1,5 +1,7 @@
 package com.dj.hotel.web;
 
+import com.dj.hotel.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user/")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 }
