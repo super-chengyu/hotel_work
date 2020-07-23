69cc676fc94582631c40d1aa56182d2573af1d3c
ycy <15703500211@163.com>
2020-07-23 09:50:34 +0800

    去登录页面的控制层

--- src/main/java/com/dj/hotel/web/page/UserPageController.java
+++ src/main/java/com/dj/hotel/web/page/UserPageController.java
@@ -1,5 +1,7 @@
 package com.dj.hotel.web.page;
 
+import com.dj.hotel.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,4 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user/")
 public class UserPageController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("toLogin")
+    public String toLogin(){
+        return "user/login";
+    }
 }
