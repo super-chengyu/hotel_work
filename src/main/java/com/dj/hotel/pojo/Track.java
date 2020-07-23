976bfc0e47d93664c43804c746efa559b9486a1f
ck <13792498255@163.com>
2020-07-23 10:24:39 +0800

    pojo里的Track轨迹表类

---
+++ src/main/java/com/dj/hotel/pojo/Track.java
@@ -0,0 +1,32 @@
+package com.dj.hotel.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * Track轨迹表
+ */
+@Data
+@TableName("track")
+public class Track {
+
+    /** 主键ID */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /** 记录表(recondite)的id */
+    private Integer recoId;
+
+    /** 菜品表(menu)的id */
+    private Integer menuId;
+
+    /** 菜品表(menu)的数量 */
+    private Integer menuNum;
+
+    /** 菜品表(menu)的价格 */
+    private Double menuPrice;
+
+
+}
