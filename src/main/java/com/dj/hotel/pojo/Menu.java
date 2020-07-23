27309d5e021cfedee2c54c64d0cb733c91e5f677
ck <13792498255@163.com>
2020-07-23 10:19:32 +0800

    pojo里的Menu菜品表类

---
+++ src/main/java/com/dj/hotel/pojo/Menu.java
@@ -0,0 +1,36 @@
+package com.dj.hotel.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * Menu菜品表
+ */
+@Data
+@TableName("menu")
+public class Menu {
+
+    /** 主键ID */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /** 菜名 */
+    private Integer menuName;
+
+    /** 菜品价格 */
+    private Double menuPrice;
+
+    /** 菜品介绍 */
+    private String menuNote;
+
+    /** 菜名状态 */
+    private Integer menuStatus;
+
+    /** 伪删除，0：正常，1：删除 */
+    private Integer isDel;
+
+
+
+}
