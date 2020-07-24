9952e27ee83f615d2e47180f1e1bf7b20a936897
ycy <15703500211@163.com>
2020-07-24 11:45:12 +0800

    点餐记录的初次提交

---
+++ src/main/java/com/dj/hotel/mapper/ReconditeMapper.java
@@ -0,0 +1,11 @@
+package com.dj.hotel.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dj.hotel.pojo.Recondite;
+
+/**
+ * 点餐记录mapper接口
+ * @author 杨承雨
+ */
+public interface ReconditeMapper extends BaseMapper<Recondite> {
+}
