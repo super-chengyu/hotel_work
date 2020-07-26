86b7b9254ff440b0f01205cb12ba558cf1e008bb
ck <13792498255@163.com>
2020-07-26 09:22:09 +0800

    用户如果订单已完成，加注解和魔法值

--- src/main/java/com/dj/hotel/web/ReconditeController.java
+++ src/main/java/com/dj/hotel/web/ReconditeController.java
@@ -1,18 +1,13 @@
 package com.dj.hotel.web;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dj.hotel.common.ResultModel;
 import com.dj.hotel.common.SysConstant;
-import com.dj.hotel.pojo.Home;
 import com.dj.hotel.pojo.Recondite;
 import com.dj.hotel.pojo.User;
 import com.dj.hotel.service.ReconditeService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -44,36 +39,12 @@ public class ReconditeController {
      * @return: map
      * @throws
      */
-//    @RequestMapping("showRecondite")
-//    public ResultModel<Object> showRecondite(Recondite recondite, Integer pageNo, Integer id){
-//        Map<String, Object> map = new HashMap<>();
-//        try {
-//            QueryWrapper<Recondite> queryWrapper = new QueryWrapper<>();
-//            queryWrapper.eq("eat_status", 10);
-//            IPage<Recondite> page = new Page<>(pageNo, SysConstant.HOME_PAGE_SIZE);
-//           // List<Recondite> reconditeById = reconditeService.findReconditeById(recondite);
-//            IPage<Recondite> pageInfo = reconditeService.page(page, queryWrapper);
-//            map.put("reconditeList", pageInfo.getRecords());
-//            map.put("pages", pageInfo.getPages());
-//            return new ResultModel<>().success(map);
-//        } catch (Exception e){
-//            e.printStackTrace();
-//            return new ResultModel<>().error("服务器异常");
-//        }
-//    }
-
-    /**
-     * 展示
-     */
     @RequestMapping("showRecondite")
     public ResultModel<Object> showRecondite(Recondite recondite, Integer pageNo, @SessionAttribute User user) {
         Map<String, Object> map = new HashMap<String, Object>();
         try {
-            //QueryWrapper<Recondite> queryWrapper = new QueryWrapper<>();
-            PageHelper.startPage(pageNo, 5);
-            //queryWrapper.eq("eat_status", 10);
+            PageHelper.startPage(pageNo, SysConstant.RECONDITE_PAGE_SIZE);
             List<Recondite> list = reconditeService.findReconditeById(recondite, user);
-            //reconditeService.page(page, queryWrapper);
             PageInfo<Recondite> pageInfo = new PageInfo<>(list);
             map.put("reconditeList", pageInfo.getList());
             map.put("pages", pageInfo.getPages());
